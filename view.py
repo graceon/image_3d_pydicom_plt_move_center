2ee467b98b21c2a34130ac7fb9ea38b222110c94
graceon <48937984+graceon@users.noreply.github.com>
2020-04-09 13:46:33 +0000

    Update view.py

--- view.py
+++ view.py
@@ -12,12 +12,14 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 ToTensor=transforms.ToTensor()
 
 def get_pixels_hu(slices):
+
     image = np.stack(slices)
+    print(image.sum())
     # 转换为int16，int16是ok的，因为所有的数值都应该 <32k
     image = image.astype(np.int16)
 
     # 设置边界外的元素为0
-    image[image == -2000] = 0
+    #image[image == -2000] = 0
 
     print(image.shape)
     # 转换为HU单位
@@ -26,8 +28,8 @@ def get_pixels_hu(slices):
         # intercept = slices[slice_number].RescaleIntercept
         # slope = slices[slice_number].RescaleSlope
 
-        intercept = 0.1
-        slope = 100
+        intercept = 25500
+        slope = 25500
 
 
         if slope != 1:
@@ -35,16 +37,17 @@ def get_pixels_hu(slices):
             image[slice_number] = image[slice_number].astype(np.int16)
 
         image[slice_number] += np.int16(intercept)
-
+    print('hu',image.shape)
+    print(image.sum())
     return np.array(image, dtype=np.int16)
 def resample(image, scan, new_spacing=[1,1,1]):
     # Determine current pixel spacing
-    SliceThickness=1
-    PixelSpacing=1
+    #SliceThickness=0.1
+    #PixelSpacing=1
     #spacing = np.array([scan[0].SliceThickness] + scan[0].PixelSpacing, dtype=np.float32)
-    spacing = np.array(np.array(SliceThickness) + PixelSpacing, dtype=np.float32)
+    spacing = np.array(np.array([2,10,10]), dtype=np.float32)
     
-    print(spacing )
+    print('spacing',spacing)
 
     resize_factor = spacing / new_spacing
     new_real_shape = image.shape * resize_factor
@@ -54,6 +57,8 @@ def resample(image, scan, new_spacing=[1,1,1]):
 
     image = scipy.ndimage.interpolation.zoom(image, real_resize_factor, mode='nearest')
 
+
+    print('re',image.shape,new_spacing)
     return image, new_spacing
 
 
@@ -63,7 +68,7 @@ def plot_3d(image, threshold=-300):
     # Position the scan upright, 
     # so the head of the patient would be at the top facing the camera
     p = image.transpose(2,1,0)
-
+    print(p.sum())
     verts, faces = measure.marching_cubes_classic(p, threshold)
 
     #fig = plt.figure(figsize=(10, 10))
@@ -72,7 +77,12 @@ def plot_3d(image, threshold=-300):
     fig = plt.figure()
     ax = Axes3D(fig)
     # Fancy indexing: `verts[faces]` to generate a collection of triangles
-    mesh = Poly3DCollection(verts[faces], alpha=0.70)
+    print(faces.shape)
+    print(verts.shape)
+    print(verts[faces].shape)
+    print(p.sum())
+
+    mesh = Poly3DCollection(verts[faces], alpha=1)
     face_color = [0.45, 0.45, 0.75]
     mesh.set_facecolor(face_color)
     ax.add_collection3d(mesh)
@@ -84,17 +94,18 @@ def plot_3d(image, threshold=-300):
     plt.show()
 
 if __name__ =="__main__":
-	path='./train/'
+	path='./modset/'
 
 
 
 	last_sum=0
 	image=[]
-	for i in range(100):
-		now_slice=(ToTensor(Image.open(path+('%05d'%i)+'_mask.png'))[0]*255)
-		sum = torch.sum(now_slice)/255
+	g=0
+	for i in range(g,g+20):
+		now_slice=(ToTensor(Image.open(path+('%05d'%i)+'_mask.png'))[0])
+		sum = torch.sum(now_slice)
 		now_slice=now_slice.numpy().tolist()
-		if(last_sum!=sum):
+		if(last_sum!=sum) or True:
 			print(i,sum)
 			last_sum=sum
 			image.append(now_slice)
@@ -102,11 +113,14 @@ if __name__ =="__main__":
 			continue
 
 	#image= (torch.stack([one,one,one],dim=0)*255).numpy()
-
+	print('orin')
+	#print(torch.sum(image))
 	first_patient = image
-	first_patient_pixels = get_pixels_hu(image)
-
+	first_patient_pixels = get_pixels_hu(first_patient )
 	pix_resampled, spacing = resample(first_patient_pixels, first_patient, [1,1,1])
+
+
+	print('pix_resampled',pix_resampled.sum())
 	plot_3d(pix_resampled, threshold=-300)
 # if __name__ =="__main__":
 # 	path='./train/'

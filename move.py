4ab8f79b8532cacbe7e8d9217529585b29017ae2
graceon <48937984+graceon@users.noreply.github.com>
2020-04-10 14:42:49 +0800

    Update move.py

--- move.py
+++ move.py
@@ -6,6 +6,10 @@ from skimage import measure, morphology
 import numpy as np
 import scipy.ndimage
 import cv2
+
+
+import time
+
 #from matplotlib import pyplot
 from torchvision.transforms import transforms
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
@@ -20,6 +24,9 @@ if __name__ =="__main__":
 	for i in range(2000):
 		now_slice=(ToTensor(Image.open(path+('%05d'%i)+'_mask.png'))[0])
 		now_sum = int(torch.sum(now_slice.int()))
+		if(now_sum<20):
+			print('point<20',i)
+			continue
 		if not(last_sum-now_sum<1 and last_sum-now_sum>-1):
 			last_sum=now_sum
 
@@ -28,32 +35,45 @@ if __name__ =="__main__":
 			img_gen=np.zeros((80,80))
 			sx = 0
 			sy = 0
+
+
+
+			point_cnt=0
+			sum_h=0
+			sum_w=0
 			for h in range(80):
 				for w in range(80):
 					if(now_slice[h][w] == 1):
-						sx = h
-						break
-			for w in range(80):
-				for h in range(80):
-					if(now_slice[h][w] == 1):
-						sy = w
-						break
-			print(sx,sy)
+						point_cnt+=1
+						sum_w+=w
+						sum_h+=h
+
+			sx=sum_w//point_cnt
+			sy=sum_h//point_cnt
+				# if(find==1):
+				# 	break
+
+				# if(find==1):
+				# 	break
+
+			# print(sx,sy)
+			# plt.imshow(now_slice,cmap='Greys_r')
+			# plt.show()
+			# time.sleep(1)
 			for h in range(-40, 40):
 				for w in range(-40, 40):
-					if sx +h < 0 or sy + w < 0 or sx + h >79 or sy + w >79 :
+					if sx +w < 0 or sy + h < 0 or sx + w >79 or sy + h >79 :
 						img_gen[40+h][40+w] = 0
 					else :
 						#print([sx+h-20],[sy+w-20])
 						# print([sx+h-20],[sy+w-20])
 						# print(img_gen.shape)
-						img_gen[40+h][40+ w] = now_slice[sx+h][sy+w]*255
-			path_gen='/fdisk/22/modset/'+str(move_cnt).zfill(5)+'_mask.png'
+						img_gen[40+h][40+w] = now_slice[sy+h][sx+w]*255
+			path_gen='/fdisk/22/modset2/'+str(move_cnt).zfill(5)+'_mask.png'
+			print(i)
 
 			cv2.imwrite(path_gen,img_gen)
 			move_cnt+=1
 		else :
 			continue
-	print(move_cnt,len(img_set))
-	img_set.sort()
-	image= (torch.stack([one,one,one],dim=0)*255).numpy()
+	print(move_cnt)
